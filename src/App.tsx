75ccb9086641e17d5cb4fe5a1b9982adea9570ea
MowiliMi <38068391+MowiliMi@users.noreply.github.com>
2020-09-17 13:46:50 +0200

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import * as filestack from "filestack-js";
 
 function App() {
   const pickerOptions = {};
-
   const open = async () => {
     const client = filestack.init("");
     const picker = await client.picker(pickerOptions);
     picker.open();
   };
-
   open();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
